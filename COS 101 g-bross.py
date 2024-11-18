2ae19a4daa394610dcf6ab1c6f92d7fbdb1e76f7
G-bross14 <oyifiodag@gmail.com>
2024-11-18 02:48:24 +0100

    Add files via upload

---
+++ COS 101 g-bross.py
@@ -0,0 +1,29 @@
+# Kinetic Energy = 1/2 × mass × velocity^2)
+mass = float(input('enter mass: '))
+velocity = float(input('enter velocity: '))
+kineticenergy = 1 / 2 * mass * velocity ** 2
+print('the kineticenergy of the object is = ', kineticenergy)
+
+# Force = mass * acceleration
+mass = float(input('enter mass: '))
+acceleration = float(input('enter acceleration: '))
+Force = mass * acceleration
+print('the force of the object is = ', Force)
+
+# Voltage = current * resistance
+current = float(input('enter current: '))
+resistance = float(input('enter resistance: '))
+Voltage = current * resistance
+print('the Voltage is = ', Voltage)
+
+# Wave Speed = frequency * wavelenght
+frequency = float(input('enter frequency: '))
+wavelenght = float(input('enter wavelenght: '))
+Wavespeed = frequency * wavelenght
+print('the Wavespeed is = ', Wavespeed)
+
+# Velocity = distance / time
+distance = float(input('enter distance: '))
+time = float(input('enter time: '))
+Velocity = distance / time
+print('the Velocity is = ', Velocity)
